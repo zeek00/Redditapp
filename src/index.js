7d8df9750079e9f504006b5807ce0c1a43e0af44
zeek00 <44764168+zeek00@users.noreply.github.com>
2023-07-23 10:39:20 +0100

    modified file structure added dependencies and setup tests with jest this commit closes issues #2 #8 #9

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+      <App />
+    
   </React.StrictMode>
 );
 
