a5ee26bc6708e3e3fec2cf88cd52ae27cab5b07a
zeek00 <44764168+zeek00@users.noreply.github.com>
2023-07-25 09:32:06 +0100

    modifications to data.js file

--- src/api/data.js
+++ src/api/data.js
@@ -20,18 +20,43 @@ const urlWithParams = `${url}?${authParams.toString()}`;
 const getData = async ()=>{
     try {
         const response = await fetch(urlWithParams, {headers: headers});
-
         if(!response.ok){
             throw new Error("Request failed with status code: " + response.status);
         }
-
         const data = await response.json();
-
-        console.log(data);
+       return  data.data.children
+        
     } catch (error) {
         console.log('This is the error: \n'+ error);
     }
 
 };
 
-getData()
+  
+const sortData = async ()=>{
+    const bigData = await getData()
+    bigData.map(onePost => {
+        
+       const sortedPost = {
+            subreddit: onePost.data.subreddit,
+            title: onePost.data.title,
+            score: onePost.data.score,
+            name: onePost.data.name,
+            ups: onePost.data.ups,
+            downs: onePost.data.downs,
+            imageUrl: onePost.data.preview.images[0].source.url,
+            post_hint: onePost.data.post_hint,
+            subbreddit_subscribers: onePost.data.subbreddit_subscribers,
+            id: onePost.data.id,
+            num_comments: onePost.data.num_comments,
+            author: onePost.data.author
+        }
+        return sortedPost
+        console.log('sorted post: ' + JSON.stringify(sortedPost));
+        
+    })
+};
+
+
+const data = sortData();
+console.log(data)
